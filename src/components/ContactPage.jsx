3a5489321167f7e9ff1eaece5959838b367f65dc
Luis <luispazdev@gmail.com>
2023-09-26 10:54:00 -0500

    adding new features to contactpage

--- src/components/ContactPage.jsx
+++ src/components/ContactPage.jsx
@@ -1,141 +1,160 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
-import { Badge, Button, Form, Row, Col, Container } from "react-bootstrap";
+import { useState } from "react"
+import { Link } from "react-router-dom"
+import { Badge, Button, Form, Row, Col, Container } from "react-bootstrap"
+import axios from "axios"
 
 export const ContactPage = () => {
-  const [input, setInput] = useState({});
+    const [input, setInput] = useState({})
 
-  const handleChange = (event) => {
-    const name = event.target.name;
-    const value = event.target.value;
-    setInput((values) => ({ ...values, [name]: value }));
-  };
+    const handleChange = (event) => {
+        const name = event.target.name
+        const value = event.target.value
+        setInput((values) => ({ ...values, [name]: value }))
+    }
 
-  const clearForm = () => {
-    document.getElementById("form").reset();
-  };
+    const clearForm = () => {
+        document.getElementById("form").reset()
+    }
 
-  return (
-    <Container
-      fluid
-      className="d-flex flex-column justify-content-center align-items-center"
-    >
-      <Row>
-        <Col xs={12} md={12} lg={12} className="text-center mt-5 mb-4">
-          <div>
-            <h1>
-              <Badge pill className="bg-danger">
-                <strong>
-                  <i>Contact Us</i>
-                </strong>
-              </Badge>
-            </h1>
-          </div>
-          <div className="mt-3">
-            <h6>
-              <i>We are here to help you </i>
-            </h6>
-          </div>
-        </Col>
-      </Row>
+    const postMessage = async () => {
+        try {
+            const response = await axios.post(
+                "https://easyshoes.onrender.com/user/contact",
+                input
+            )
+            console.log(response)
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
-      <Form
-        style={{ backgroundColor: "#212529" }}
-        className="mt-3 border border-dark rounded p-4"
-        id="form"
-      >
-        <Row>
-          <Col xs={12} md={6} lg={6} className="mb-3">
-            <Form.Group controlId="formBasicName">
-              <Form.Label>
-                <h6>
-                  <strong>
-                    <i>Name</i>
-                  </strong>
-                </h6>
-              </Form.Label>
-              <Form.Control
-                className="border border-dark"
-                onChange={handleChange}
-                name="name"
-                type="text"
-                placeholder="Enter your Name"
-                required
-              />
-            </Form.Group>
-          </Col>
+    // send data to backend
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        console.log("message sent", input)
+        postMessage()
+        clearForm()
+    }
 
-          <Col xs={12} md={6} lg={6} className="mb-3">
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label>
-                <h6>
-                  <strong>
-                    <i>Email</i>
-                  </strong>
-                </h6>
-              </Form.Label>
-              <Form.Control
-                className="border border-dark"
-                onChange={handleChange}
-                name="email"
-                type="email"
-                placeholder="Enter email"
-                required
-              />
-            </Form.Group>
-          </Col>
+    return (
+        <Container
+            fluid
+            className='d-flex flex-column justify-content-center align-items-center'>
+            <Row>
+                <Col xs={12} md={12} lg={12} className='text-center mt-5 mb-4'>
+                    <div>
+                        <h1>
+                            <Badge pill className='bg-danger'>
+                                <strong>
+                                    <i>Contact Us</i>
+                                </strong>
+                            </Badge>
+                        </h1>
+                    </div>
+                    <div className='mt-3'>
+                        <h6>
+                            <i>We are here to help you </i>
+                        </h6>
+                    </div>
+                </Col>
+            </Row>
 
-          <Col xs={12} md={12} lg={12} className="text-start mt-3">
-            <Form.Group controlId="formBasicDescription">
-              <Form.Label>
-                <h6>
-                  <strong>
-                    <i>Message</i>
-                  </strong>
-                </h6>
-              </Form.Label>
-              <Form.Control
-                as="textarea"
-                rows={5}
-                className="border border-dark input-comment"
-                onChange={handleChange}
-                name="comment"
-                type="text"
-                placeholder="Leave your Message here"
-                required
-              />
-            </Form.Group>
-          </Col>
+            <Form
+                style={{ backgroundColor: "#212529" }}
+                className='mt-3 border border-dark rounded p-4'
+                id='form'>
+                <Row>
+                    <Col xs={12} md={6} lg={6} className='mb-3'>
+                        <Form.Group controlId='formBasicName'>
+                            <Form.Label>
+                                <h6>
+                                    <strong>
+                                        <i>Name</i>
+                                    </strong>
+                                </h6>
+                            </Form.Label>
+                            <Form.Control
+                                className='border border-dark'
+                                onChange={handleChange}
+                                name='name'
+                                type='text'
+                                placeholder='Enter your Name'
+                                required
+                            />
+                        </Form.Group>
+                    </Col>
 
-          <div className="text-center mt-3">
-            <Button
-              variant="outline-light"
-              className="mt-3 mb-3"
-              size="lg"
-              type="submit"
-            >
-              <strong>
-                <i>Send</i>
-              </strong>
-            </Button>
-          </div>
-        </Row>
-      </Form>
+                    <Col xs={12} md={6} lg={6} className='mb-3'>
+                        <Form.Group controlId='formBasicEmail'>
+                            <Form.Label>
+                                <h6>
+                                    <strong>
+                                        <i>Email</i>
+                                    </strong>
+                                </h6>
+                            </Form.Label>
+                            <Form.Control
+                                className='border border-dark'
+                                onChange={handleChange}
+                                name='email'
+                                type='email'
+                                placeholder='Enter email'
+                                required
+                            />
+                        </Form.Group>
+                    </Col>
 
-      <Row className="ms-auto me-auto mt-5 mb-5">
-        <Col xs={12} md={12} lg={12} className="text-center">
-          <Link to="/">
-            <h5 className="text-center text-black">
-              Go to{" "}
-              <strong>
-                <i>
-                  <u> HOME </u>
-                </i>
-              </strong>
-            </h5>
-          </Link>
-        </Col>
-      </Row>
-    </Container>
-  );
-};
+                    <Col xs={12} md={12} lg={12} className='text-start mt-3'>
+                        <Form.Group controlId='formBasicDescription'>
+                            <Form.Label>
+                                <h6>
+                                    <strong>
+                                        <i>Message</i>
+                                    </strong>
+                                </h6>
+                            </Form.Label>
+                            <Form.Control
+                                as='textarea'
+                                rows={5}
+                                className='border border-dark input-comment'
+                                onChange={handleChange}
+                                name='comment'
+                                type='text'
+                                placeholder='Leave your Message here'
+                                required
+                            />
+                        </Form.Group>
+                    </Col>
+
+                    <div className='text-center mt-3'>
+                        <Button
+                            onClick={handleSubmit}
+                            variant='outline-light'
+                            className='mt-3 mb-3'
+                            size='lg'
+                            type='submit'>
+                            <strong>
+                                <i>Send</i>
+                            </strong>
+                        </Button>
+                    </div>
+                </Row>
+            </Form>
+
+            <Row className='ms-auto me-auto mt-5 mb-5'>
+                <Col xs={12} md={12} lg={12} className='text-center'>
+                    <Link to='/'>
+                        <h5 className='text-center text-black'>
+                            Go to{" "}
+                            <strong>
+                                <i>
+                                    <u> HOME </u>
+                                </i>
+                            </strong>
+                        </h5>
+                    </Link>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
--- src/context/userContext.jsx
+++ src/context/userContext.jsx
@@ -1,105 +1,115 @@
-import { useState, createContext, useEffect } from "react";
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { useState, createContext, useEffect } from "react"
+import axios from "axios"
+import { useNavigate } from "react-router-dom"
+import Swal from "sweetalert2"
 
-export const UserContext = createContext();
+export const UserContext = createContext()
 
 export const UserProvider = ({ children }) => {
-  const navigate = useNavigate();
-  const [user, setUser] = useState({
-    username: "",
-    email: "",
-    password: "",
-  });
-  const [authStatus, setAuthStatus] = useState(false);
+    const navigate = useNavigate()
+    const [user, setUser] = useState({
+        username: "",
+        email: "",
+        password: "",
+    })
+    const [authStatus, setAuthStatus] = useState(false)
 
-  const [formData, setFormData] = useState({
-    username: "",
-    email: "",
-    password: "",
-  });
+    const [formData, setFormData] = useState({
+        username: "",
+        email: "",
+        password: "",
+    })
 
-  const handleChange = (event) => {
-    event.preventDefault();
-    setFormData({
-      ...formData,
-      [event.target.name]: event.target.value,
-    });
-  };
-
-  const onResetForm = () => {
-    setFormData({
-      username: "",
-      email: "",
-      password: "",
-    });
-  };
-
-  const registerUser = async (dataForm) => {
-    try {
-      await axios.post(
-        "https://easyshoes.onrender.com/user/register",
-        dataForm
-      );
-      onResetForm();
-      // Navigate to login page
-      navigate("/login");
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const verifyingToken = async () => {
-    const token = localStorage.getItem("token");
-
-    if (token) {
-      axios.defaults.headers.common["x-auth-token"] = token;
-    } else {
-      delete axios.defaults.headers.common["x-auth-token"];
+    const handleChange = (event) => {
+        event.preventDefault()
+        setFormData({
+            ...formData,
+            [event.target.name]: event.target.value,
+        })
     }
 
-    try {
-      const res =
-        token && (await axios.get("https://easyshoes.onrender.com/user/get"));
-      setUser(res.data.data);
-      setAuthStatus(true);
-    } catch (error) {
-      console.log("token error", error);
+    const onResetForm = () => {
+        setFormData({
+            username: "",
+            email: "",
+            password: "",
+        })
     }
-  };
 
-  const loginUser = async (dataForm) => {
-    try {
-      const res = await axios.post(
-        "https://easyshoes.onrender.com/login",
-        dataForm
-      );
-      localStorage.setItem("token", res.data.token);
-      verifyingToken();
-      onResetForm();
-    } catch (error) {
-      alert("Incorrect email or password!! Please try again or register");
-      console.log(error);
+    const registerUser = async (dataForm) => {
+        try {
+            await axios.post(
+                "https://easyshoes.onrender.com/user/register",
+                dataForm
+            )
+            onResetForm()
+            // Navigate to login page
+            navigate("/login")
+        } catch (error) {
+            console.log(error)
+        }
     }
-  };
 
-  const logout = () => {
-    localStorage.removeItem("token");
-    setUser(null);
-    setAuthStatus(false);
-  };
+    const verifyingToken = async () => {
+        const token = localStorage.getItem("token")
 
-  const data = {
-    registerUser,
-    loginUser,
-    handleChange,
-    verifyingToken,
-    logout,
-    formData,
-    setFormData,
-    user,
-    authStatus,
-  };
+        if (token) {
+            axios.defaults.headers.common["x-auth-token"] = token
+        } else {
+            delete axios.defaults.headers.common["x-auth-token"]
+        }
 
-  return <UserContext.Provider value={data}>{children}</UserContext.Provider>;
-};
+        try {
+            const res =
+                token &&
+                (await axios.get("https://easyshoes.onrender.com/user/get"))
+            setUser(res.data.data)
+            setAuthStatus(true)
+        } catch (error) {
+            console.log("token error", error)
+        }
+    }
+
+    const loginUser = async (dataForm) => {
+        try {
+            const res = await axios.post(
+                "https://easyshoes.onrender.com/login",
+                dataForm
+            )
+            localStorage.setItem("token", res.data.token)
+            verifyingToken()
+            onResetForm()
+        } catch (error) {
+            Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "Email or password is incorrect!",
+            })
+            console.log(error)
+        }
+    }
+
+    const logout = () => {
+        localStorage.removeItem("token")
+        setUser(null)
+        setAuthStatus(false)
+    }
+
+    useEffect(() => {
+        verifyingToken()
+    }, [])
+
+    const data = {
+        registerUser,
+        loginUser,
+        handleChange,
+        verifyingToken,
+        logout,
+        formData,
+        setFormData,
+        user,
+        authStatus,
+    }
+
+    return <UserContext.Provider value={data}>{children}</UserContext.Provider>
+}
